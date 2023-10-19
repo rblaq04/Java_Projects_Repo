09179e08b710d6e688039584a9d158df08fde42b
rblaq04 <147408868+rblaq04@users.noreply.github.com>
2023-10-19 16:42:34 +0100

    Update and rename W6-Project.java to grp4_Assignment1.java
    Used printf to format the time output instead of if/else statements. Also added more comments, and renamed the file.

---
+++ grp4_Assignment1.java
@@ -0,0 +1,17 @@
+public class grp4_Assignment1 {
+    public static void main(String[] args) {
+        long midMins = (System.currentTimeMillis() % (24*60*60*1000) / 1000) / 60; // Minutes since midnight last night
+        long midSecs = (System.currentTimeMillis() % (24*60*60*1000) / 1000); // Seconds since midnight last night
+        long curHour = (midMins/60); // Current hour of the day
+        long curMin = (midMins)-(curHour*60); // Minutes since last hour
+        long curSec = (midSecs)-((curHour*60*60)+(curMin*60)); // Seconds since last minute
+        
+        String msg, poster; // Defining variables for the content of the message and the poster
+        poster = System.getProperty("user.name");
+        msg = "Cats are banned from this WhatsApp group \uD83D\uDEAB \uD83D\uDE3C";
+        
+        System.out.println("Posted by: " + poster);
+        System.out.println(msg);
+        System.out.printf("Posted at: %02d:%02d:%02d%n%n", (curHour+1), curMin, curSec); // Time output with formatting to add zero if needed
+    }
+}
